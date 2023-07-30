fb1579ee002f312e22446a0583ab632f6e03d5f8
jja6312 <127708710+jja6312@users.noreply.github.com>
2023-07-31 00:41:02 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,39 +1,105 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[]args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        String str = br.readLine();
-        int length = str.length();
-        int count = 0;
+        String A = br.readLine();
 
-        for (int i = 0; i < length; i++) {
-            char ch = str.charAt(i);
-            if (ch == 'c' && i < length - 1) {
-                if (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {
-                    i++;
-                }
-            } else if (ch == 'd' && i < length - 1) {
-                if (str.charAt(i + 1) == '-') {
-                    i++;
-                } else if (i < length - 2 && str.charAt(i + 1) == 'z' && str.charAt(i + 2) == '=') {
-                    i += 2;
-                }
-            } else if ((ch == 'l' || ch == 'n') && i < length - 1) {
-                if (str.charAt(i + 1) == 'j') {
-                    i++;
-                }
-            } else if ((ch == 's' || ch == 'z') && i < length - 1) {
-                if (str.charAt(i + 1) == '=') {
-                    i++;
-                }
+        int check=0;
+        int count=0;
+        while(true){
+            int index = A.indexOf("c=", check);
+            if (index == -1) {
+                break;
+            } else {
+                check = index + 2;
+                count++;
             }
-            count++;
         }
 
-        bw.write(Integer.toString(count));
+        int check2=0;
+        while(true){
+            int index = A.indexOf("c-", check2);
+            if (index == -1) {
+                break;
+            } else {
+                check2 = index + 2;
+                count++;
+            }
+        }
+
+        int check3=0;
+        int count2=0;
+        while(true){
+            int index = A.indexOf("dz=", check3);
+            if (index == -1) {
+                break;
+            } else {
+                check3 = index + 3;
+                count2++;
+            }
+        }
+
+        int check4=0;
+        while(true){
+            int index = A.indexOf("d-", check4);
+            if (index == -1) {
+                break;
+            } else {
+                check4 = index + 2;
+                count++;
+            }
+        }
+
+        int check5=0;
+        while(true){
+            int index = A.indexOf("lj", check5);
+            if (index == -1) {
+                break;
+            } else {
+                check5 = index + 2;
+                count++;
+            }
+        }
+
+        int check6=0;
+        while(true){
+            int index = A.indexOf("nj", check6);
+            if (index == -1) {
+                break;
+            } else {
+                check6 = index + 2;
+                count++;
+            }
+        }
+
+        int check7=0;
+        while(true){
+            int index = A.indexOf("s=", check7);
+            if (index == -1) {
+                break;
+            } else {
+                check7 = index + 2;
+                count++;
+            }
+        }
+
+        int check8=0;
+        int count3=0;
+        while(true){
+            int index = A.indexOf("z=", check8);
+            if (index == -1) {
+                break;
+            } else {
+                check8 = index + 2;
+                count3++;
+            }
+        }
+
+        int result = A.length() - count - count2*2 - (count3 - count2);
+        bw.write(""+result);
         bw.flush();
         bw.close();
         br.close();
