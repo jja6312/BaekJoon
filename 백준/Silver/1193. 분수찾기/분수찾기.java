0362bf572aec69c51f6ab940f6549b23d2c2321f
jja6312 <127708710+jja6312@users.noreply.github.com>
2023-09-19 21:48:07 +0900

    [Silver V] Title: 분수찾기, Time: 144 ms, Memory: 15948 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -2,46 +2,44 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[]args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        long X = Long.valueOf(br.readLine());
-        
-        long countSum=0;
-        long i=0;
-        while(X>countSum){
-            i++;
-            countSum += i;
-        }
-        
-        if(i%2==0){
-            bw.write(""+(i-(countSum-X))+"/"+(1+(countSum-X)));
-        }else{
-            bw.write(""+(1+(countSum-X))+"/"+(i-(countSum-X)));
-        }
+        read();
         bw.flush();
         bw.close();
         br.close();
-        /*
-        
-        
-        <풀었는데 위에서 아래로 이동하는 로직으로 짰다..ㅠ 왓다갓다구나>
-        X=1, i=1, countSum =1
-        X=3, i=1, cS=1
-             i=2, cS=3
-             
-             이게 i=1에서는 분자분모 합이 2고, i=2에서는 합이 3이고, ... 반복한다.
-             X가 8이라면, 8은 i의 몇번째인지 체크하자.
-             그럼 i값이 나올것이다.
-             그리고 그 i값에서 countSum값 - X값으로, 순서를 구하자.
-             만약 X=5라고 했을 때, countSum은 123의 합 6이다.
-             이 때, countSum - X = 1이다.
-             즉, 6번째 값은 가장 왼쪽 끝에 있는 값이고, 거기서 한 칸 당긴 값을 구하면 된다.
-             123은 i가 3이다.
-             즉, 분자+분모 = 4인 값이다.
-             가장 왼쪽의 숫자는 i-(countSum-X)/1+(countSum-X)
-             
-             
-        */
+
+    }//main()
+
+    public static void read() throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int X = 0;
+        while (sum < N) {
+            X++;
+            sum += X;
+        }
+
+        if (N == 1) {
+            bw.write("1/1");
+        } else {
+            int min = sum - X;  // 여기를 수정했습니다.
+            int realIndex = N - (min + 1);
+
+            int[] arr = new int[X];
+            for (int i = 0; i < X; i++) {
+                arr[i] = i + 1;
+            }
+
+            if (X % 2 == 0) {
+                bw.write(arr[realIndex] + "/");
+                bw.write(arr[(X - 1) - realIndex] + "");
+            } else {
+                bw.write(arr[(X - 1) - realIndex] + "/");
+                bw.write(arr[realIndex] + "");
+            }
+        }
     }
-}
+}//Main
