7b1522e81d636f4aad49089bec4c9bcbb619f322
jja6312 <127708710+jja6312@users.noreply.github.com>
2023-09-20 09:28:55 +0900

    [Silver V] Title: 분수찾기, Time: 140 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -13,33 +13,47 @@ public class Main{
 
     }//main()
 
-    public static void read() throws IOException {
+    public static void read() throws IOException{
         int N = Integer.parseInt(br.readLine());
-        int sum = 0;
-        int X = 0;
-        while (sum < N) {
-            X++;
-            sum += X;
+        /*X를 구하자.
+        X 는 , N번째가 속하는 배열의 순서이다.
+        */
+        int sum = 0; //sum은 N이속한 배열의 최고값의 순서.
+        int X=0; //X는 몇번째 배열인지 판독하는것
+        while(sum<N) {
+        	X++;
+        	sum+=X;
         }
-
-        if (N == 1) {
-            bw.write("1/1");
-        } else {
-            int min = sum - X;  // 여기를 수정했습니다.
-            int realIndex = N - (min + 1);
-
-            int[] arr = new int[X];
-            for (int i = 0; i < X; i++) {
-                arr[i] = i + 1;
-            }
-
-            if (X % 2 == 0) {
-                bw.write(arr[realIndex] + "/");
-                bw.write(arr[(X - 1) - realIndex] + "");
-            } else {
-                bw.write(arr[(X - 1) - realIndex] + "/");
-                bw.write(arr[realIndex] + "");
-            }
+        
+        if(N==1){
+        	bw.write("1/1");
+        }else {
+        	int min = sum - X;
+        	
+	        	
+	        //index 번째 배열이라고 볼수있다.
+	        int[] arr = new int[X];
+	        for(int i=0; i<X; i++){
+	            arr[i]=i+1;
+	        }
+	        int realIndex = N-min-1;
+	        
+	        if(N==min) {
+	        	bw.write(arr[X-1]+"/1");
+	        	return;
+	        }
+	        if(X%2==0){
+	            //분자
+	            bw.write(arr[realIndex]+"/");
+	            //분모
+	            bw.write(arr[(X-1)-realIndex]+"");
+	        }else{
+	            //분자
+	            bw.write(arr[(X-1)-realIndex]+"/");
+	            //분모
+	            bw.write(arr[realIndex]+"");
+	        }
         }
-    }
+        
+    }//read()
 }//Main
