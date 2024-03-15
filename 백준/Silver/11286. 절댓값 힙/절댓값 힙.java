3b5d6e2183e1c10ca71bf1cb5b21d9411bd0bee5
정지안 <127708710+jja6312@users.noreply.github.com>
2024-03-15 17:33:33 +0900

    [Silver I] Title: 절댓값 힙, Time: 364 ms, Memory: 27332 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,28 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    public static void main(String[]args) throws IOException{
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> MyQueue = new PriorityQueue<>((o1,o2)->{
-            int first_abs = Math.abs(o1);
-            int second_abs = Math.abs(o2);
-            if(first_abs==second_abs) return o1>o2 ? 1:-1;
-            else return first_abs - second_abs;
-        });
-        for(int i=0; i<N; i++){
-            int request = Integer.parseInt(br.readLine());
-            if(request==0){
-                if(MyQueue.isEmpty()){
-                    System.out.println("0");
-                }else{
-                    System.out.println(MyQueue.poll());
-                } 
-            }else{
-                    MyQueue.add(request);
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue(
+                new Comparator<Integer>() {
+                    @Override
+                    public int compare(Integer o1, Integer o2) {
+                        if(Math.abs(o1)==Math.abs(o2)) return o1 > o2 ? 1 : -1;
+                        return Math.abs(o1)-Math.abs(o2);
+                    }
                 }
-            
-        }
+        );
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++){
+            int x = Integer.parseInt(br.readLine());
+            if(x == 0 && !pq.isEmpty()){
+                sb.append(pq.poll()+"\n");
+            }else if(x==0 && pq.isEmpty()){
+                sb.append("0\n");
+            }else if(x!=0){
+                pq.offer(x);
+            }
+        }//for
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
+
 }
