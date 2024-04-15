ccb0b7c1a1f2e6db2184165e4207fa37b8ae49eb
정지안 <127708710+jja6312@users.noreply.github.com>
2024-04-15 17:39:51 +0900

    [Bronze V] Title: We love kriii, Time: 112 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.util.List;
+
+
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        bw.write("강한친구 대한육군\n");
+        bw.write("강한친구 대한육군\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }//main
+
+}//Main
