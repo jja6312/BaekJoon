ac4777bfd0f552aa50344546608d071691b3f51a
정지안 <127708710+jja6312@users.noreply.github.com>
2024-05-05 13:50:11 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 15156 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,57 @@
+import javax.xml.crypto.dsig.keyinfo.KeyValue;
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+
+    public static void main(String[] args) throws IOException {
+        //초기화
+        int n = Integer.parseInt(br.readLine());
+        int cnt = bfs(n);
+        bw.write(cnt+"");
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    public static int bfs(int n){
+        Queue<int[]> queue = new LinkedList<>();
+        boolean [] visited = new boolean[n+1]; //방문 확인
+        queue.offer(new int[]{n,0}); // {현재 숫자, 현재 레벨}
+        visited[n] = true;
+
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int currentNum = current[0];
+            int level = current[1];
+
+            if(currentNum == 1) return level;
+
+            if(currentNum % 3 == 0 && !visited[currentNum/3]){
+                visited[currentNum/3] = true;
+                queue.offer(new int[]{currentNum/3,level+1});
+            }
+            if(currentNum % 2 == 0 && !visited[currentNum/2]){
+                visited[currentNum / 2] = true;
+                queue.offer(new int[] {currentNum/2, level+1});
+            }
+            if(currentNum > 1 && !visited[currentNum-1]){
+                visited[currentNum-1] = true;
+                queue.offer(new int[]{currentNum-1,level+1});
+
+            }
+        }
+        return -1;
+    }
+
+
+
+}//Main Class
