70090dedbb6d32d6a3dff51a448ee46e43756ce8
정지안 <127708710+jja6312@users.noreply.github.com>
2024-05-06 13:35:21 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 15156 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -11,8 +11,8 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-        //초기화
         int n = Integer.parseInt(br.readLine());
+
         int cnt = bfs(n);
         bw.write(cnt+"");
 
@@ -23,35 +23,39 @@ public class Main {
     }
 
     public static int bfs(int n){
-        Queue<int[]> queue = new LinkedList<>();
-        boolean [] visited = new boolean[n+1]; //방문 확인
-        queue.offer(new int[]{n,0}); // {현재 숫자, 현재 레벨}
+        Deque<int[]> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[n+1];
+
+        queue.offer(new int[]{n,0});
         visited[n] = true;
 
         while(!queue.isEmpty()){
+
             int[] current = queue.poll();
             int currentNum = current[0];
-            int level = current[1];
+            int level =  current[1];
 
-            if(currentNum == 1) return level;
+            if(currentNum==1) return level;
 
-            if(currentNum % 3 == 0 && !visited[currentNum/3]){
+            if(currentNum%3==0 && !visited[currentNum/3]){
                 visited[currentNum/3] = true;
-                queue.offer(new int[]{currentNum/3,level+1});
+                queue.offer(new int[]{currentNum/3, level+1});
             }
-            if(currentNum % 2 == 0 && !visited[currentNum/2]){
-                visited[currentNum / 2] = true;
-                queue.offer(new int[] {currentNum/2, level+1});
+
+            if(currentNum%2 == 0 && !visited[currentNum/2]){
+                visited[currentNum/2] = true;
+                queue.offer(new int[]{currentNum/2, level+1});
             }
-            if(currentNum > 1 && !visited[currentNum-1]){
+
+            if(currentNum>1 && !visited[currentNum-1]){
                 visited[currentNum-1] = true;
                 queue.offer(new int[]{currentNum-1,level+1});
-
             }
+
         }
+
+
         return -1;
     }
-
-
 
 }//Main Class
