2b6a311c4a5080b389e8ffffc7db450c23972c03
정지안 <127708710+jja6312@users.noreply.github.com>
2024-05-21 11:01:48 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1440 ms, Memory: 321708 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -5,25 +5,23 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int[] parent;
-    static ArrayList<Edge> edgeList;
 
-    static class Edge implements Comparable<Edge> {
+    static class Edge implements Comparable<Edge>{
         int start;
         int end;
         int weight;
 
-        public Edge(int start, int end, int weight) {
+        public Edge(int start, int end, int weight){
             this.start = start;
             this.end = end;
             this.weight = weight;
         }
 
         @Override
-        public int compareTo(Edge o) {
-            return weight - o.weight;
+        public int compareTo(Edge edge){
+            return weight-edge.weight;
         }
     }
-
 
     public static void main(String[] args) throws IOException {
         // 최소비용 :  크루스칼 알고리즘
@@ -32,38 +30,34 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        edgeList = new ArrayList<>();
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-
-            edgeList.add(new Edge(start, end, weight));
-
-        }
-
         parent = new int[n+1];
+        //parent 초기화
         for(int i=1; i<=n; i++){
             parent[i] = i;
         }
 
-        Collections.sort(edgeList);
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
 
-        //모든 집이 n-1개의 길로 연결되도록 만듦
+        //가중치가 낮은 순서대로 큐에 삽입
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            Edge edge = new Edge(start,end,weight);
+            pq.offer(edge);
+        }
+
         int ans = 0;
         int bigCost = 0;
-        for(int i=0; i< edgeList.size(); i++){
-            Edge edge = edgeList.get(i);
-
-            //사이클이 발생하는 간선은 제외
-            if(find(edge.start)!=find(edge.end)){
-                ans+= edge.weight;
-                union(edge.start, edge.end);
-
+        //최소 신장 트리로 연결한다.
+        while(!pq.isEmpty()){
+            Edge edge = pq.poll();
+            if(find(edge.start)!=find(edge.end)){//순환하는 구조가 아닐때
+                ans+= edge.weight;// 연결
                 bigCost = edge.weight;
+
+                union(edge.start,edge.end);
             }
         }
         bw.write(ans-bigCost+"\n");
@@ -73,21 +67,19 @@ public class Main {
         br.close();
     }
 
+    public static int find(int node){
+        //노드가 최상위 노드일때 반환
+        if(node == parent[node]) return node;
 
-    public static int find(int x){
-        if(x==parent[x]){
-            return x;
-        }
-        return parent[x] = find(parent[x]);
+        //아니라면, 크루스칼 알고리즘을 통해 경로를 압축한다.
+        return parent[node] = find(parent[node]);
     }
 
-    public static void union(int x, int y){
-        x = find(x);
-        y = find(y);
+    public static void union(int start, int end){
 
-        if(x!=y){
-            parent[y] = x;
-        }
+        int root1 = find(start);
+        int root2 = find(end);
+        parent[root1] = root2;//한쪽을 다른쪽루트로 결합
     }
 
 }
