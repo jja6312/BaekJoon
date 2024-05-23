176b2f7bdf0e818840e28a162cbacbf8d276ea9d
정지안 <127708710+jja6312@users.noreply.github.com>
2024-05-23 14:13:19 +0900

    [Gold V] Title: 수 나누기 게임, Time: 512 ms, Memory: 34596 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -1,3 +1,6 @@
+import org.w3c.dom.Node;
+
+import javax.sound.sampled.Line;
 import java.io.*;
 import java.util.*;
 
@@ -5,48 +8,52 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int n;
-    static int NOT_CARD = 777777777;
+    static int NOT_CARD=777777777;
 
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int max = 0;
-        for (int i = 0; i < n; i++) {
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int max=0;
+        for(int i=0; i<n ;i++){
             arr[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(max, arr[i]);
+            max = Math.max(max,arr[i]);
         }
 
-        int[] point = new int[max + 1];
-        Arrays.fill(point, NOT_CARD);
+        int[] point = new int[max+1];
+        Arrays.fill(point,NOT_CARD);
 
-        for (int i = 0; i < n; i++) {
-            int card = arr[i];
-            point[card] = 0;
+        for(int i=0; i<n; i++){
+            int card = arr[i];//카드는
+            point[card] = 0;// 카드가 있는 배열에 순번을 매긴다.
         }
+        //즉 point배열의 card번째에, 카드의 점수가 있다.
+        //나머지는 NOT_CARD로 되어있다.
 
-        for (int i = 1; i <= max; i++) {
-            if (point[i] == NOT_CARD) continue;
+        for(int i=1; i<=max; i++){
+            //만약 NOT_CARD라면 넘어가기
+            if(point[i]==NOT_CARD) continue;
 
-            int cnt = 0;
-            for (int cardMul = 2 * i; cardMul <= max; cardMul += i) {
-                if (point[cardMul] == NOT_CARD) continue;
+            int cnt=0;
+            //카드라면, 그 배수들에 - 해준다.
+            for(int cardMul=2*i; cardMul<=max; cardMul+=i){
+                if(point[cardMul]==NOT_CARD) continue;
                 point[cardMul]--;
                 cnt++;
             }
+            //연산을 끝내고, 카운팅을 본인에게 +해준다.
             point[i] += cnt;
         }
 
         int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
             result[i] = point[arr[i]];
+            bw.write(result[i]+" ");
         }
-
-        for (int i = 0; i < n; i++) {
-            bw.write(result[i] + " ");
-        }
+        
         bw.flush();
         bw.close();
         br.close();
-    }
-}
+    }//main()
+
+}//Main Class
