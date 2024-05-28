605fc7eb92b399cfa5bd18f90ae68822a953670c
정지안 <127708710+jja6312@users.noreply.github.com>
2024-05-28 10:36:57 +0900

    [Silver I] Title: IOIOI, Time: 276 ms, Memory: 20180 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -9,27 +9,28 @@ class Main {
         int n  = Integer.parseInt(br.readLine());
         int m  = Integer.parseInt(br.readLine());
         String S = br.readLine();
-        String p = "";
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("IOI");
-        if(n!=1){
-            for(int i=1; i<n; i++){
-                sb.append("OI");
-            }
-        }
-        p = sb.toString();
-
-        //만약 p가 S에 포함되어 있다면
-        //S = p의 시작위치 +2, 문자열끝까지 subString
+        
 
         int cnt=0;
-        while(S.indexOf(p)!=-1){
-            int idx = S.indexOf(p);
-            S = S.substring(idx+2,S.length());
-            cnt++;
+        int patternCnt = 0;
+        for(int i=0; i<m-2; i++){
+            if(S.charAt(i)=='I' && S.charAt(i+1)=='O' && S.charAt(i+2)=='I'){
+                cnt++;
+                if(cnt==n){
+                    patternCnt++;
+                    cnt--;
+                }
+                i++;//다음위치로 이동
+            }else{
+                cnt = 0;
+            }
+
         }
-        bw.write(cnt+"");
+
+
+
+
+        bw.write(patternCnt+"");
 
 
         bw.flush();
