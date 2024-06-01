845ee8d044156fab618d9f2722676c640147dddc
정지안 <127708710+jja6312@users.noreply.github.com>
2024-06-01 23:08:51 +0900

    [Silver IV] Title: 가로수, Time: 284 ms, Memory: 23472 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.java
+++ 백준/Silver/2485. 가로수/가로수.java
@@ -1,79 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
+class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int N;
-    static ArrayList<Integer> arrList;
-    static Integer[] distance;
-    static Integer[] neededTree;
-    
-    public static void main(String[]args) throws IOException{
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
 
-    	read();
-        arrListAdd();
-        int minDistance = minDistance();
-    	neededTree = new Integer[N-1];
-        
-    	int result=0;
-    	int realMinDistance;
-    	ArrayList <Integer>arrList2 = new ArrayList <Integer>();
-    	for(int i=0; i<N-1; i++) {
-    		
-    			realMinDistance= gcd(distance[i],minDistance);
-    			arrList2.add(realMinDistance);
-    		
-    	}
-    	Collections.sort(arrList2);
-    	int realRealMinDistance = arrList2.get(1);
-        
-        	for(int i=0; i<N-1; i++){
-        	neededTree[i] =distance[i]/realRealMinDistance-1;
-        	result+=neededTree[i];
-        	}
-        
-        
+        // 간격 계산
+        int gcdVal = arr[1] - arr[0];
+        for (int i = 1; i < n - 1; i++) {
+            int diff = arr[i + 1] - arr[i];
+            gcdVal = gcd(gcdVal, diff);
+        }
 
-        bw.write(""+result);
+        int totalNewTrees = 0;
+        for (int i = 0; i < n - 1; i++) {
+            totalNewTrees += (arr[i + 1] - arr[i]) / gcdVal - 1;
+        }
+
+        bw.write(totalNewTrees + "\n");
         bw.flush();
         bw.close();
         br.close();
-        
-        
-        
-    }//main()
-    
-    public static void read() throws IOException{
-    	
-        N = Integer.parseInt(br.readLine());
-    }//read()
-    
-    public static void arrListAdd()throws IOException{
-        arrList = new ArrayList<Integer>();
-        for(int i=0; i<N; i++){
-            int treeIndex = Integer.parseInt(br.readLine());
-            arrList.add(treeIndex);
-        }
-    }//arrListAdd
-    
-    public static int minDistance(){
-        distance = new Integer[N-1];
-        for(int i = 0; i < N-1; i++) {
-            distance[i] = arrList.get(i+1) - arrList.get(i);
-        }
-        Arrays.sort(distance);
-        return distance[0];
-    }//minDistance
-    
-    public static int gcd(int a, int b) {
-    	while(b!=0) {
-    		int remainder = a%b;
-    		a = b;
-    		b = remainder;
-    	}
-    	return a;
     }
-    
-}//Main
+
+    private static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
