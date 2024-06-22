bf1e1ee1f664b9033b35fd87e1501e780d8be31f
정지안 <127708710+jja6312@users.noreply.github.com>
2024-06-22 10:14:40 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -8,50 +8,60 @@ public class Main {
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int L = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        char[] arr = new char[C];
-        for (int i = 0; i < C; i++) {
+        st= new StringTokenizer(br.readLine());
+        char[] arr = new char[K];
+        for(int i=0; i<K; i++){
             arr[i] = st.nextToken().charAt(0);
         }
 
-        Arrays.sort(arr);  // 알파벳 순으로 정렬
-        combination(arr, new char[L], 0, 0, L);
+        Arrays.sort(arr);
+
+        List<String> result = new ArrayList<>();
+
+        combination(arr,new char[L],0,0,L);
+
+        for(String x: result){
+            bw.write(x+"\n");
+        }
+
+
         bw.flush();
         bw.close();
         br.close();
     }
 
-    private static void combination(char[] arr, char[] chosen, int start, int depth, int L) throws IOException {
-        if (depth == L) {
-            if (isValid(chosen)) {
-                bw.write(chosen);
+    private static void combination(char[] arr, char[] chars, int start, int depth, int L) throws IOException {
+        if(depth==L){
+            if(isValid(chars)){
+                for(char x:chars){
+                    bw.write(x);
+                }
                 bw.write("\n");
             }
             return;
         }
 
-        for (int i = start; i < arr.length; i++) {
-            chosen[depth] = arr[i];
-            combination(arr, chosen, i + 1, depth + 1, L);
+        for(int i=start; i<arr.length; i++){
+            chars[depth] = arr[i];
+            combination(arr,chars,i+1,depth+1,L);
         }
+
+
     }
 
-    private static boolean isValid(char[] chosen) {
-        int vowels = 0;
-        int consonants = 0;
-        for (char c : chosen) {
-            if (isVowel(c)) {
-                vowels++;
-            } else {
-                consonants++;
-            }
+    public static boolean isValid(char[] chars){
+        int 자음 = 0;
+        int 모음 = 0;
+
+        for(char x:chars){
+            if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u') 모음++;
+            else 자음++;
         }
-        return vowels >= 1 && consonants >= 2;
+        
+        return 모음>=1 && 자음>=2;
     }
 
-    private static boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-    }
+
 }
