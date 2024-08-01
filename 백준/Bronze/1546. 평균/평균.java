5c51a838c886f9d4d50962958f8f159f4a6be01f
정지안 <127708710+jja6312@users.noreply.github.com>
2024-08-01 22:34:41 +0900

    [Bronze I] Title: 평균, Time: 136 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,37 +1,47 @@
-import java.util.*;
 import java.io.*;
-import java.text.*;
+import java.util.*;
 
-public class Main{
-    public static void main(String []args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        Integer N = Integer.valueOf(br.readLine());
-        
-        Integer [] arr = new Integer[N];
-        for(int i=0; i<N; i++){
-            arr[i] = i+1;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+        String A = br.readLine();
+        StringTokenizer st = new StringTokenizer(A, " ");
+
+        int m = 0;
+        int [] arr = new int[n];
+        for(int i=0; i<n; i++){
+            int score = Integer.parseInt(st.nextToken());
+            arr[i] = score;
+            m = Math.max(m,score);
         }
-        
-        
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.valueOf(st.nextToken());
+
+        double [] miracleArr = new double[n];
+
+        double sum=0;
+        for(int i=0; i<n; i++){
+            miracleArr[i] = miracleCal(m,arr[i]);
+            sum+=miracleArr[i];
         }
-        Arrays.sort(arr);
-        int max = arr[N-1];
-        int sum=0;
-        for(int i=0; i<N; i++){
-            sum += arr[i];
-        }
-        double avg = (1.0*sum/N)/max*100;
-        DecimalFormat df = new DecimalFormat("#.##");
-        String avgS = df.format(avg);
-        bw.write(avgS);
+        double answer = ((int)(sum*100/n))/100.0;
+        bw.write(""+answer);
         bw.flush();
         bw.close();
         br.close();
+
+    }//main()
+
+    public static double miracleCal(int m, int score){
+        double answer =100.0*score/m;
+        return answer;
     }
-}
+
+
+
+}//Main Class
+
