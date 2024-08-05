8701c573204a91c23c3fd1e1049d7bd4fbca430e
정지안 <127708710+jja6312@users.noreply.github.com>
2024-08-05 15:39:51 +0900

    [Silver II] Title: DNA 비밀번호, Time: 348 ms, Memory: 23168 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,101 +1,69 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
+public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int checkArr[];
-    static int myArr[];
-    static int checkSecret;
 
-    public static void main(String[]args) throws IOException{
-        read();
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st;
+        String A = br.readLine();
+        st = new StringTokenizer(A, " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int range = Integer.parseInt(st.nextToken());
+
+        String B = br.readLine();
+
+        int[] conditionArr = new int[4];
+        String C = br.readLine();
+        st = new StringTokenizer(C, " ");
+        for(int i=0; i<4; i++){
+            conditionArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int lt = 0;
+        int rt = range - 1;
+        int cnt = 0;
+        int[] checkArr = new int[4];
+        for(int i=lt; i<=rt; i++){
+            if(B.charAt(i)=='A') checkArr[0]++;
+            else if(B.charAt(i)=='C') checkArr[1]++;
+            else if(B.charAt(i)=='G') checkArr[2]++;
+            else if(B.charAt(i)=='T') checkArr[3]++;
+        }
+
+        while(rt < n){ // 수정된 부분: 종료 조건을 `rt < n`으로 변경
+            boolean isTrue = true;
+            for(int i=0; i<4; i++){
+                if(conditionArr[i] > checkArr[i]){
+                    isTrue = false;
+                    break;
+                }
+            }
+            if(isTrue) cnt++;
+
+            if(rt + 1 < n){ // 마지막 윈도우 처리를 위한 조건 추가
+                if(B.charAt(lt) == 'A') checkArr[0]--;
+                else if(B.charAt(lt) == 'C') checkArr[1]--;
+                else if(B.charAt(lt) == 'G') checkArr[2]--;
+                else if(B.charAt(lt) == 'T') checkArr[3]--;
+                lt++;
+
+                rt++;
+                if(B.charAt(rt) == 'A') checkArr[0]++;
+                else if(B.charAt(rt) == 'C') checkArr[1]++;
+                else if(B.charAt(rt) == 'G') checkArr[2]++;
+                else if(B.charAt(rt) == 'T') checkArr[3]++;
+            } else {
+                // 마지막 문자까지 처리가 끝났으므로 루프를 빠져나옵니다.
+                break;
+            }
+        }
+
+        bw.write(""+cnt);
         bw.flush();
         bw.close();
         br.close();
-
-    }//main()
-
-    public static void read() throws IOException{
-    	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int S = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
-        int Result = 0;
-        char A[] = new char[S];
-        checkArr = new int[4];
-        myArr = new int [4];
-        checkSecret = 0;
-        A=br.readLine().toCharArray();
-        st= new StringTokenizer(br.readLine());
-        for(int i=0; i<4; i++){
-            checkArr[i] = Integer.parseInt(st.nextToken());
-            if(checkArr[i] ==0){
-                checkSecret++;
-            }
-        }
-            for(int i=0; i<P; i++){
-                Add(A[i]);
-            }
-            if(checkSecret ==4) Result++;
-            //슬라이딩 윈도우 처리
-            for(int i=P; i<S; i++){
-                int j = i-P;
-                Add(A[i]);
-                Remove(A[j]);
-                if(checkSecret==4) Result++;
-            }
-            bw.write(Result+"");
-            
-        }
-        private static void Add(char c){
-            switch(c){
-                case 'A':
-                    myArr[0]++;
-                    if(myArr[0] == checkArr[0]) {
-                        checkSecret++;
-                    }
-                    break;
-                case 'C':
-                    myArr[1]++;
-                    if(myArr[1]==checkArr[1]){
-                        checkSecret++;
-                    }
-                    break;
-                case 'G':
-                    myArr[2]++;
-                    if(myArr[2]==checkArr[2]){
-                         checkSecret++;
-                    }
-                    break;
-                    
-                case 'T':
-                    myArr[3]++;
-                    if(myArr[3]==checkArr[3]){
-                         checkSecret++;
-                    }
-                    break;
-            }//switch
-        }
-
-        private static void Remove(char c){//제거되는 문자를 처리하는 함수
-            switch(c){
-                case'A':
-                    if(myArr[0]==checkArr[0]) checkSecret--;
-                    myArr[0]--;
-                    break;
-                case'C':
-                    if(myArr[1]==checkArr[1]) checkSecret--;
-                    myArr[1]--;
-                    break;
-                case'G':
-                    if(myArr[2]==checkArr[2]) checkSecret--;
-                    myArr[2]--;
-                    break;
-                case'T':
-                    if(myArr[3]==checkArr[3]) checkSecret--;
-                    myArr[3]--;
-                    break;
-            }//case
-        }
-
-    }//read()
+    }
+}
