3b94c6c46e97d41492ba2fe9e57c7653ab452583
정지안 <127708710+jja6312@users.noreply.github.com>
2024-08-06 09:37:22 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 580 ms, Memory: 142164 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,62 +1,56 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int count = 0;
-    static boolean visited[];
-    static int N;
-    static int M;
-    static ArrayList<Integer>[] A1;
+    static boolean[] isvisited;
+    static ArrayList<Integer>[] listArr;
 
-    public static void main(String args[]) throws IOException {
-        read();
-        bw.write(count + "");
+
+    public static void main(String[] args) throws IOException {
+        String A = br.readLine();
+        StringTokenizer st = new StringTokenizer(A, " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        isvisited = new boolean[n + 1];
+        listArr = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            listArr[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            String str = br.readLine();
+            st = new StringTokenizer(str, " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            listArr[start].add(end);
+            listArr[end].add(start);
+        }
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            if(isvisited[i]) continue;
+            cnt++;
+            dfs(i);
+        }
+
+        bw.write(""+cnt);
+
+
         bw.flush();
         bw.close();
         br.close();
-    }
 
-    public static void read() throws IOException {
-        String A = br.readLine();
-        StringTokenizer st = new StringTokenizer(A, " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        visited = new boolean[N + 1];
-        A1 = new ArrayList[N + 1];
+    }//main
 
-        for (int i = 1; i <= N; i++) {
-            A1[i] = new ArrayList<>();
+    private static void dfs(int num){
+        if(isvisited[num]) return;
+
+        isvisited[num] = true;
+        for(int x: listArr[num]){
+            dfs(x);
         }
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            A1[s].add(e);
-            A1[e].add(s);
-        }
-
-        for (int i = 1; i <= N; i++) {
-            if (!visited[i]) { // DFS를 시작하기 전에 방문 여부를 확인
-                count++;
-                DFS(i);
-            }
-        }
-    }
-
-    static void DFS(int v) {
-        visited[v] = true;
-        for (int i : A1[v]) {
-            if (!visited[i]) {
-                DFS(i);
-            }
-        }
-    }
+    }//dfs
 }
