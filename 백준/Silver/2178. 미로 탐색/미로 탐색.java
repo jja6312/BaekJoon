ff775805c8fa9194f9477420e3eaa648c822372f
정지안 <127708710+jja6312@users.noreply.github.com>
2024-11-01 10:48:39 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14784 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,102 +1,92 @@
-import java.awt.*;
-import java.io.*;
-import java.util.*;
-
-class Point {
-    private int x;
-    private int y;
-    private int distance;
-
-    public Point(int y, int x, int distance) {
-        this.y = y;
-        this.x = x;
-        this.distance = distance;
-    }
-
-    public int getX() {
-        return this.x;
-    }
-
-    public int getY() {
-        return this.y;
-    }
-
-    public int getDistance(){
-        return this.distance;
-    }
-
-}
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    static int n;
-    static int m;
-    static int answer;
-    static boolean[][] isVisited;
-    static int[][] arr;
-
-
-    public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        arr = new int[n][m];
-        isVisited = new boolean[n][m];
-
-
-        //갔던 길은 가지 않고 최소값을 구하자.
-
-        //1. 초기화
-        for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt("" + str.charAt(j));
-            }//for2
-        }//for1
-
-        answer=bfs(0,0);
-
-
-        bw.write("" + answer);
-        bw.flush();
-        bw.close();
-        br.close();
-
-
-    }//main
-
-    private static int bfs(int y, int x) {
-        Deque<Point> deque = new ArrayDeque();
-        deque.add(new Point(y, x, 1));
-        isVisited[y][x] = true;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int n;
+	static int m;
+	static int[][] arr;
+	static boolean[][] visited;
+	
+	
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st =new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n][m];
+		visited = new boolean[n][m];
+		
+		for(int i=0; i<n;i++) {
+			String A = br.readLine();
+			for(int j=0; j<m; j++) {
+				arr[i][j] = A.charAt(j)-'0';
+			}
+		}
+		
+		
+		int min = bfs();
+		StringBuilder sb = new StringBuilder();
+		sb.append(min);
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+		
+		
+		
+	}
 
 
+	private static int bfs() {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.offer(new int[] {0,0,1}); // y, x, cnt
+		visited[0][0] = true;
+		
+		int[] dx = {0,0,-1,1};
+		int[] dy = {-1,1,0,0};
+		
+		while(!queue.isEmpty()) {
+			int[]current = queue.poll();
+			int y = current[0];
+			int x = current[1];
+			int cnt = current[2];
+			
+			if(y ==n-1 && x == m-1) {
+				return cnt;
+			}
+			
+			for(int i=0; i<4; i++) {
+				int moveY = dy[i] + y;
+				int moveX = dx[i] + x;
+				
+				
+				if(isBoundary(moveY,moveX) && !visited[moveY][moveX] && arr[moveY][moveX]==1) {
+					
+					visited[moveY][moveX] = true;
+					queue.offer(new int[] {moveY,moveX,cnt+1});
+				}
+				
+			}
+			
+			
+		}
+		
+		
+		return -1;
+	}
 
-        while (!deque.isEmpty()) {
-            Point p = deque.poll();
 
-            if (p.getY() == n - 1 && p.getX() == m - 1) return p.getDistance();
+	private static boolean isBoundary(int y, int x) {
 
-            //상하좌우로 이동
-            int[] dx = {0, 0, -1, 1};
-            int[] dy = {1, -1, 0, 0};
-            for (int i = 0; i < 4; i++) {
-                int moveX = dx[i] + p.getX();
-                int moveY = dy[i] + p.getY();
-
-                if (moveX < m && moveX >= 0 && moveY < n && moveY >= 0 && !isVisited[moveY][moveX]  && arr[moveY][moveX] == 1) {
-                    
-                    deque.add(new Point(moveY, moveX, p.getDistance()+1));
-                    isVisited[moveY][moveX] = true;
-                }
-
-            }//for
-        }//while
-
-        return -1; // 도달할 수 없는 경우
-    }//bfs
-}//MainClass
+		return y>=0 && y<n && x>=0 && x<m;
+	}
+}
