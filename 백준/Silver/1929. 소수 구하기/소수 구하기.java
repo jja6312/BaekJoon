d55d2221c7e16876e9e110e25a9cea28c6c10bf1
정지안 <127708710+jja6312@users.noreply.github.com>
2024-11-01 22:03:09 +0900

    [Silver III] Title: 소수 구하기, Time: 264 ms, Memory: 22348 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,37 +1,40 @@
-import java.util.*;
-import java.io.*;
 
-public class Main{
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int N;
-    static int M;
-    
-    public static void main(String[]args) throws IOException{
-    	read();
-        for(int i=N; i<=M; i++){
-            if(isSosu(i)==true) bw.write(i+" ");
+    static int m,n;
+    static boolean[] isPrime;
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer st =  new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        isPrime = new boolean[n+1];
+        for(int i=2; i<=n; i++){
+            isPrime[i] = true;
         }
+
+        // m 이상 n 이하 소수구하기.
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(isPrime[i]){
+                for(int j=i*i; j<=n; j+=i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for(int i=m; i<=n; i++){
+            if(isPrime[i]){
+                bw.write(i+"\n");
+            }
+        }
+
         bw.flush();
         bw.close();
         br.close();
-    	
-    }//main
-    
-    public static void read() throws IOException{
-        String A = br.readLine();
-        String[] arrA = A.split(" ");
-        N = Integer.parseInt(arrA[0]);
-        M = Integer.parseInt(arrA[1]);
-    }//read()
-    
-    public static boolean isSosu(int n){
-        if(n<=1) return false;
-        else{
-            for(int i=2; i<=Math.sqrt(n); i++){
-                if (n%i==0) return false;
-            }
-        }//else
-        return true;
     }
-}//Main
+}
